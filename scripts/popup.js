7055378df1bada06cb674c8f94ef575710b1c437
PinkyJie <pilixiaoxuanfeng@gmail.com>
2014-02-11 21:45:05 +0800

    using local time; add tip for non-liangzi-user

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,4 @@
-function genFileName() {
+function getTimeStr() {
     var date = new Date();
     return [
         date.getFullYear(),
@@ -14,8 +14,10 @@ function genUrlFromCookie(cookie, count) {
     return 'http://api.linezing.com/?' + count + '&_s=' + cookie.lzsession;
 }
 
-function downloadStat(cookie) {
-    alert('test!');
+function downloadStat(user, session) {
+    var fileName = encodeURIComponent(user) + '-' + getTimeStr() + '.csv';
+    var url = "https://lzd.ap01.aws.af.cm?file=" + fileName + '&token=' + session;
+    window.open(url);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -23,14 +25,22 @@ document.addEventListener('DOMContentLoaded', function () {
     var unloginDiv = document.querySelector('#unlogin'),
         loginedDiv = document.querySelector('#logined'),
         userSpan = document.querySelector('#user'),
+        tipSpan = document.querySelector('#tip'),
         downloadBtn = document.querySelector('#download');
     if (cookie.lzsession && cookie.lznick) {
         unloginDiv.style.display = 'none';
         loginedDiv.style.display = 'block';
-        userSpan.textContent = cookie.lznick;
-        downloadBtn.addEventListener('click', function(){
-            downloadStat(cookie);
-        });
+        userSpan.textContent = decodeURI(cookie.lznick);
+        if (cookie.lzcat === '3') {
+            tipSpan.style.display = 'block';
+            downloadBtn.style.display = 'none';
+        } else {
+            tipSpan.style.display = 'none';
+            downloadBtn.style.display = 'inline';
+            downloadBtn.addEventListener('click', function(){
+                downloadStat(userSpan.textContent, cookie.lzsession);
+            });
+        }
     } else {
         unloginDiv.style.display = 'block';
         loginedDiv.style.display = 'none';
