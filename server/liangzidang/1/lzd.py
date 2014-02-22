7afd6f4fcb65452ac7eb570ad9ad0126de092ba2
PinkyJie <pilixiaoxuanfeng@gmail.com>
2014-02-23 00:13:56 +0800

    backend support for download only chosen fileds

--- server/liangzidang/1/lzd.py
+++ server/liangzidang/1/lzd.py
@@ -2,6 +2,7 @@
 
 import json
 import urllib
+import collections
 
 from flask import (Flask, request, make_response)
 
@@ -23,23 +24,39 @@ def get_data(token):
     res2_json = json.loads(res2)
     return res2_json[1]
 
-def gen_csv(data):
+def gen_csv(data, fields):
+    header_map = collections.OrderedDict([
+        ('day', u'访问日期'),
+        ('log_time', u'访问时间'),
+        ('ref_type', u'入店来源'),
+        ('ref', u'来源网址'),
+        ('title', u'被访页面'),
+        ('url', u'页面网址'),
+        ('ip', u'访客IP'),
+        ('location_name', u'访客位置'),
+        ('uv_no', u'顾客跟踪'),
+        ('uv_return', u'是否回头客'),
+        ('uv', u'顾客UV')
+    ])
     content = []
-    content.append(u'序号, 访问日期, 访问时间, 入店来源, 来源网址, 被访页面, 页面网址, 访客IP, 访客位置, 顾客跟踪, 是否回头客, 顾客UV')
+    if len(fields) == 0:
+        fields = header_map.keys()
+    header = [u'序号']
+    for x in fields:
+        header.append(header_map[x])
+    content.append(u' ,'.join(header))
     content.append(u'\n')
     for i, res in enumerate(data):
         content.append(u'%d, ' % (i+1))
-        content.append(u'%s, ' % res['day'])
-        content.append(u'%s, ' % res['log_time'])
-        content.append(u'%s, ' % res['ref_type'])
-        content.append(u'%s, ' % res['ref'])
-        content.append(u'%s, ' % res['title'])
-        content.append(u'%s, ' % res['url'])
-        content.append(u'%s, ' % res['ip'])
-        content.append(u'%s, ' % res['location_name'])
-        content.append(u'%s%d, ' % (u'顾客', res['uv_no']))
-        content.append(u'%s, ' % (u'回访' if res['uv_return'] == 1 else u'新'))
-        content.append(res['uv'])
+        for x in fields:
+            if x == 'uv_no':
+                content.append(u'%s%d, ' % (u'顾客', res[x]))
+            elif x == 'uv_return':
+                content.append(u'%s, ' % (u'回访' if res[x] == 1 else u'新'))
+            elif x == 'uv':
+                content.append(res[x])
+            else:
+                content.append(u'%s, ' % res[x])
         content.append('\n')
     return u''.join(content)
 
@@ -50,7 +67,10 @@ def root():
     file_name = request.args.get('file')
     token = request.args.get('token')
     data = get_data(token)
-    content = gen_csv(data)
+    fields = []
+    if 'fields' in request.args:
+        fields = request.args.get('fields').split(',')
+    content = gen_csv(data, fields)
     response = make_response(content.encode('gbk'))
     response.headers['Content-type'] = 'text/csv'
     response.headers['Content-Disposition'] = "attachment;filename=" + urllib.quote(file_name.encode('utf-8'))
